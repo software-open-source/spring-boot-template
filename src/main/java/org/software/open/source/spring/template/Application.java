2871050815ffa3e01fdf4546dac68633138cd5d0
Richard Vu <richardvu.work@gmail.com>
2025-09-11 18:28:57 +0700

    Refactor docker-compose.yml and Application.java for clarity

--- src/main/java/org/software/open/source/spring/template/Application.java
+++ src/main/java/org/software/open/source/spring/template/Application.java
@@ -14,6 +14,7 @@ public class Application {
 	public static void main(String[] args) {
 		log.info("Project start at: [{}]", ZonedDateTime.now());
 		SpringApplication.run(Application.class, args);
+
 	}
 
 }
